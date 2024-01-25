84f0f2570ae13080dfa8558335b83fae92aed73a
hicham-oualla <hich2022am.oualla@gmail.com>
2024-01-25 16:28:30 +0100

    creation du menu

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main() {
+	int choix;
+	while(1){
+	printf("l'espace de gestion des taches\n\n");
+	printf("1.pour afficher la liste des taches :\n");
+	printf("2.pour ajouter une nouvelle tache:\n" );
+	printf("3.pour  Modifier une Tache:\n");
+	printf("4.pour Supprimer une Tache :\n");
+	printf("5.pour  Ordonner les Taches:\n");
+	printf("5.pour Filtrer les Taches  :\n");
+	printf("6.pour Quitter  :\n");
+	scanf("%d", &choix);
+	}
+	
+	
+	
+	
+	return 0;
+}
